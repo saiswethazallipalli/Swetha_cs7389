1fbd265b63d1a17f66618f8b580a02bc74c1dafe
saiswethazallipalli <saiswethazallipalli2001@gmail.com>
2026-02-18 11:59:07 -0600

    Update prog1.c

--- Lab2/prog1.c
+++ Lab2/prog1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    int nproc, rank;
+    int msg = -1;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (nproc < 2) {
+        if (rank == 0) {
+            printf("Need at least 2 MPI processes for prog1.\n");
+        }
+        MPI_Finalize();
+        return 0;
+    }
+
+    if (rank == 0) {
+        msg = rank; // send my id (0)
+        MPI_Send(&msg, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        printf("Rank %d sent value %d to rank 1\n", rank, msg);
+    } else if (rank == 1) {
+        MPI_Recv(&msg, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Rank %d received value %d from rank 0\n", rank, msg);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
 
